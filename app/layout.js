0c6abc803adf1cc534d9be09d9b415a5a04e97c5
fesantospalmeira <felipesantos7938@gmail.com>
2024-06-06 13:29:29 -0300

    add favicon.ico + layout.js changes

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
